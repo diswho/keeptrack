c35bcc24ff0680f6cfd22a68c41781474dbfabbc
diswho <discover.who@gmail.com>
2023-06-21 10:09:58 +0700

    completed Lab 13

--- src/projects/ProjectForm.tsx
+++ src/projects/ProjectForm.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { Project } from './Project';
 
-function ProjectForm() {
+interface ProjectFormProps {
+  project: Project;
+  onCancel: () => void;
+}
+
+function ProjectForm({ project, onCancel }: ProjectFormProps) {
   return (
     <form className="input-group vertical">
       <label htmlFor="name">Project Name</label>
@@ -18,7 +24,7 @@ function ProjectForm() {
       <div className="input-group">
         <button className="primary bordered medium">Save</button>
         <span />
-        <button type="button" className="bordered medium">
+        <button type="button" className="bordered medium" onClick={onCancel}>
           Cancel
         </button>
       </div>
--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Project } from './Project';
 import ProjectCard from './ProjectCard';
 import ProjectForm from './ProjectForm';
@@ -7,12 +7,26 @@ interface ProjectListProps {
   projects: Project[];
 }
 function ProjectList({ projects }: ProjectListProps) {
+  const [projectBeingEdited, setProjectBeingEdited] = useState({});
+
+  const handleEdit = (project: Project) => {
+    setProjectBeingEdited(project);
+    // console.log(project);
+  };
+
+  const cancelEdit = () => {
+    setProjectBeingEdited({});
+  };
+
   return (
     <div className="row">
       {projects.map((project) => (
         <div className="cols-sm" key={project.id}>
-          <ProjectCard project={project}></ProjectCard>
-          <ProjectForm />
+          {project === projectBeingEdited ? (
+            <ProjectForm project={project} onCancel={cancelEdit} />
+          ) : (
+            <ProjectCard project={project} onEdit={handleEdit}></ProjectCard>
+          )}
         </div>
       ))}
     </div>
