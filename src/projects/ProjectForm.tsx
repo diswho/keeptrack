f5c65f7c600b07ab4e4de473327c56a2b3ca5cb7
diswho <discover.who@gmail.com>
2023-06-24 20:09:38 +0700

    completed Lab 16

--- src/projects/ProjectForm.tsx
+++ src/projects/ProjectForm.tsx
@@ -1,5 +1,5 @@
-import React, { SyntheticEvent, useState } from 'react';
-import { Project } from './Project';
+import React, { SyntheticEvent, useState } from "react";
+import { Project } from "./Project";
 
 interface ProjectFormProps {
   project: Project;
@@ -14,9 +14,16 @@ function ProjectForm({
 }: ProjectFormProps) {
   const [project, setProject] = useState(initialProject);
 
+  const [errors, setErrors] = useState({
+    name: "",
+    description: "",
+    budget: "",
+  });
+
   const handleSubmit = (event: SyntheticEvent) => {
     event.preventDefault();
     // onSave(new Project({ name: 'Updated Project' }));
+    if (!isValid()) return;
     onSave(project);
   };
 
@@ -24,10 +31,10 @@ function ProjectForm({
     const { type, name, value, checked } = event.target;
     // if input type is checkbox use checked
     // otherwise it's type is text, number etc. so use value
-    let updatedValue = type === 'checkbox' ? checked : value;
+    let updatedValue = type === "checkbox" ? checked : value;
 
     //if input type is number convert the updatedValue string to a +number
-    if (type === 'number') {
+    if (type === "number") {
       updatedValue = Number(updatedValue);
     }
     const change = {
@@ -44,7 +51,37 @@ function ProjectForm({
       // updatedProject = new Project({ p, change });
       return updatedProject;
     });
+    setErrors(() => validate(updatedProject));
   };
+  function validate(project: Project) {
+    let errors: any = { name: "", description: "", budget: "" };
+
+    if (project.name.length === 0) {
+      errors.name = "Name is required";
+    }
+
+    if (project.name.length > 0 && project.name.length < 3) {
+      errors.name = "Name needs to be at least 3 characters.";
+    }
+
+    if (project.description.length === 0) {
+      errors.description = "Description is required.";
+    }
+
+    if (project.budget === 0) {
+      errors.budget = "Budget must be more than $0.";
+    }
+
+    return errors;
+  }
+
+  function isValid() {
+    return (
+      errors.name.length === 0 &&
+      errors.description.length === 0 &&
+      errors.budget.length === 0
+    );
+  }
 
   return (
     <form className="input-group vertical" onSubmit={handleSubmit}>
@@ -56,7 +93,11 @@ function ProjectForm({
         value={project.name}
         onChange={handleChange}
       />
-
+      {errors.name.length > 0 && (
+        <div className="card error">
+          <p>{errors.name}</p>
+        </div>
+      )}
       <label htmlFor="description">Project Description</label>
       <input
         type="text"
@@ -65,7 +106,11 @@ function ProjectForm({
         value={project.description}
         onChange={handleChange}
       />
-
+      {errors.description.length > 0 && (
+        <div className="card error">
+          <p>{errors.description}</p>
+        </div>
+      )}
       <label htmlFor="budget">Project Budget</label>
       <input
         type="number"
@@ -74,7 +119,11 @@ function ProjectForm({
         value={project.budget}
         onChange={handleChange}
       />
-
+      {errors.budget.length > 0 && (
+        <div className="card error">
+          <p>{errors.budget}</p>
+        </div>
+      )}
       <label htmlFor="isActive">Active?</label>
       <input
         type="checkbox"
