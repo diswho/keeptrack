a739eb5b257ab27f97d7f3e69aff1134c461c04a
diswho <discover.who@gmail.com>
2023-06-26 21:26:59 +0700

    completed Lab 20

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
-import './App.css';
-import ProjectsPage from './projects/ProjectsPage';
+import React from "react";
+import "./App.css";
+import ProjectsPage from "./projects/ProjectsPage";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 // test
+
 function App() {
   return (
-    <div className="App">
-      <ProjectsPage />
-    </div>
+    <Router>
+      <header className="sticky">
+        <span className="logo">
+          <img src="/assets/logo-3.svg" alt="logo" width="49" height="99" />
+        </span>
+        <NavLink to="/" className="button rounded">
+          <span className="icon-home"></span>
+          Home
+        </NavLink>
+        <NavLink to="/projects" className="button rounded">
+          Projects
+        </NavLink>
+      </header>
+      <div className="App">
+        <ProjectsPage />
+      </div>
+    </Router>
   );
 }
 
